418ded037bd23e78ebee27af2dafe9fe30a34b9c
Matthew Vernon <hello@matthewvernon.co>
2018-06-27 17:13:09 +1000

    init

---
+++ js/main.js
@@ -0,0 +1,71 @@
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 400, function() {
+          // Callback after animation
+          // Must change focus!
+          // var $target = $(target);
+          // $target.focus();
+          // if ($target.is(":focus")) { // Checking if the target was focused
+          //   return false;
+          // } else {
+          //   $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          //   $target.focus(); // Set focus again
+          // };
+        });
+      }
+    }
+  });
+
+
+
+// fade in sections  ==================================
+
+  $(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $(".fadein")
+
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i]
+
+      if ($(tag).offset().top < pageBottom) {
+        $(tag).addClass("visible")
+      } else {
+        $(tag).removeClass("visible")
+      }
+    }
+  })
+
+
+// progress bar ============
+
+$(document).on('scroll', function() {
+  var pixelsFromTop = $(document).scrollTop();
+  var documentHeight = $(document).height()
+  var windowHeight = $(window).height()
+
+  var difference = documentHeight - windowHeight
+
+  var percentage = 100 * pixelsFromTop / difference
+
+  $('#progress-bar').css('width', percentage + '%')
+
+});
